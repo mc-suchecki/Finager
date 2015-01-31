92f3f53652802d5217c6703129ad48b13433cefa
Maciej Suchecki <mc.suchecki@gmail.com>
2015-01-31 20:47:11 +0100

    Added NavigationDrawer to the Android frontend

--- android/app/src/main/java/pl/mc/finager/MainActivity.java
+++ android/app/src/main/java/pl/mc/finager/MainActivity.java
@@ -1,5 +1,6 @@
 package pl.mc.finager;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,9 +15,12 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_appbar);
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setup(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
     }
 
 
